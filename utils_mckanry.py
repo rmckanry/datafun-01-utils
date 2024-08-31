977d322ad7de60e272507709adec9f18c12078a0
rmckanry <rmckanry@stchas.edu>
2024-08-31 07:28:13 -0500

    Iteration 3
    Added variables and updated byline text

--- utils_mckanry.py
+++ utils_mckanry.py
@@ -1,34 +1,42 @@
-''' ITERATION 2
+''' ITERATION 3
 
 Module: Insights Professional Analytics - Reusable Module for My Data Analytics Projects
 
-This module provides a simple, reusable foundation for my analytics projects. 
-When we work hard to write useful code, we want it to be reusable.
-A good byline could be used in every Python analytics project we do.
+This module provides a simple, reusable foundation for my analytics projects.
 
 Process:
-
-We don't write code from top to bottom; instead, we often write it from the outside in.
-Here's what a first draft of my utils_case.py might look like:
-
-1. I start with this docstring at the very beginning.
-   I use it to clarify the purpose of my Python file and organize my thoughts.
-   
-2. I'll declare a global variable for my byline string and just set it to some simple text.
-
-3. I'll declare a main() function for my module. When I run this script, I can use main() to test my byline.
-
-4. I'll add the boilerplate conditional execution code so I only run the main() function when 
-   this script is executed directly (but not when I import it into another file).
-
-I'll test it in an online interpreter to ensure this version runs correctly before continuing.
+In this third iteration, I declared variables to show skills with different data types.
 '''
 
 #####################################
 # Declare a global variable named byline.
 #####################################
 
-byline: str = 'Insights Professional Analytics: Delivering Professional Projections and Insights'
+#Boolean variable to indicate if the company has international clients
+has_international_clients: bool = True
+
+#Integer variable for the number of years in operation
+years_in_operation: int = 10
+
+#List if strings representing the skills offered by the company
+skills_offered: list = ["Data Analytics", "Machine Learning", "Business Intelligence"]
+
+#List of floats representing individual client satisfaction scores
+client_satisfaction_scores: list = [4.8, 4.6, 4.9, 5.0, 4.7]
+
+#####################################
+# Declare a global variable named byline
+#####################################
+
+byline: str = f"""
+-------------------------------------------------------
+Stellar Analytics: Delivering Professional Insights
+-------------------------------------------------------
+Has International Clients:   (has_international_clients)
+Years of Operation:          (years_in_operation)
+Skills Offered:              (skills_offered)
+Client Satisfaction Scores:  (client_satisfaction_scores)
+"""
 
 #####################################
 # Declare get_byline function to return byline text.
@@ -40,16 +48,6 @@ def get_byline() -> str:
 #####################################
 # Define a main() function for this module.
 #####################################
-
-# Create a function named main.
-# A function is a block of code that performs a specific task.
-# This function will simply print the byline to the console.
-# Add a type hint to indicate that this function doesn't return anything when called 
-# (that is, it has a Python type of None).
-# It doesn't need any additional information passed in, 
-# so there's nothing needed inside the parentheses.
-# Everything after the colon (:) must be indented (usually 4 spaces)
-
 def main() -> None:
     '''Print the byline to the console when this function is called.'''
     print(get_byline())
